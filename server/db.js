59043c4ba44b738b2e27567ef878d5944213eb4b
Atul Himanshu <priyanshuprogrammer49@gmail.com>
2026-05-13 08:26:58 +0530

    uri removed

--- server/db.js
+++ server/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const URI = "mongodb+srv://Atul:atulhim4325b@cluster0.x0sraxy.mongodb.net/?appName=Cluster0"
-    const conn = await mongoose.connect(URI);
+    const conn = await mongoose.connect(process.env.URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
